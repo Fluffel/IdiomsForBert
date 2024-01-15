ccc19f74b1bf057cc14b8c0e808db166ac9f0181
Florian <flokandra9@gmail.com>
2024-01-15 18:52:00 +0100

    Added evaluation as bar plot

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import textwrap
 
 
 
 if __name__ == '__main__':
     with open("similarity_scores.json") as f:
         df = pd.read_json(f, orient='split')
-        print(df)
-        # plt.bar(df["similarity score"])
-        # plt.show()
+        # print(df.loc["similarity score"])
+        scores = df.loc["similarity score"]
+        x_labels = df.columns
+        # scores = scores.transpose()
+
+        # # Plotting the bar chart
+        scores.plot(kind='bar', legend=False, figsize=(12, 10))
+        wrapped_labels = [textwrap.fill(label, width=10) for label in x_labels]
+        plt.title('Similarity Scores for Sentences')
+        # plt.xlabel('Phrases')
+        plt.ylabel('Similarity Score')
+        plt.xticks(range(len(wrapped_labels)), wrapped_labels, rotation=45)
+        plt.savefig('sim_scores.png')
